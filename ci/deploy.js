4fca86a17b668478360726173c0373104228e156
Zero Liu <zerolxy@gmail.com>
2022-02-10 11:28:13 -0800

    commit

--- ci/deploy.js
+++ ci/deploy.js
@@ -2,10 +2,17 @@ const baseDomains = {
   prod: ['asset.fireworktv.com'],
   staging: ['asset-staging.fireworktv.com'],
 };
+async function fail() {
+  try {
+    throw new Error('intentional failure');
+  } catch (e) {
+    return e;
+  }
+}
 
 (async () => {
   try {
-    await console.log('in async');
+    await fail();
     console.log(baseDomains['foo'][0]);
   } catch (e) {
     console.log(e);
